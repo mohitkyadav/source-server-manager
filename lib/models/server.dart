580e0efc102bd05bf270b7447042040a8fe1fd67
Mohit K. Yadav <mohitkyadav@outlook.com>
2021-04-10 10:48:27 +0530

    server model

---
+++ lib/models/server.dart
@@ -0,0 +1,28 @@
+class Server {
+  Server(this.serverName, this.serverIp, this.serverPort, this.serverRcon,
+      this.serverGame);
+
+  String serverName;
+  String serverIp;
+  String serverPort;
+  String serverRcon;
+  String serverGame;
+
+  @override
+  String toString() {
+    return serverName;
+  }
+
+  Map<String, String> toJson() => <String, String> {
+    'serverName': serverName,
+    'serverIp': serverIp,
+    'serverPort': serverPort,
+    'serverRcon': serverRcon,
+    'serverGame': serverGame,
+  };
+
+  Server fromJson(Map<String, String> json) => Server(
+    json['serverName'], json['serverName'], json['serverName'],
+    json['serverName'], json['serverName']
+  );
+}
