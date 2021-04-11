bf9bcd21f290d2daed76ecd0662bcc67d57c0363
Mohit K. Yadav <mohitkyadav@outlook.com>
2021-04-11 08:43:26 +0530

    add server details

--- lib/ui/pages/server_details/server_details_header.dart
+++ lib/ui/pages/server_details/server_details_header.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'package:turrant/models/server.dart';
+import 'package:turrant/themes/styling.dart';
 
 class ServerDetailsHeader extends StatelessWidget {
   const ServerDetailsHeader(this.server, this.map, this.numOfPlayers,
@@ -21,18 +24,31 @@ class ServerDetailsHeader extends StatelessWidget {
               alignment: Alignment.center,
               widthFactor: 1,
               heightFactor: 0.2,
-              child: Image.asset('assets/img/de_mirage.jpg',),
+              child: Image.asset('assets/img/$map.jpg',),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text('${server.serverIp}:${server.serverPort}',
-                  style: Theme.of(context).textTheme.headline6),
-              Text(map, style: Theme.of(context).textTheme.subtitle1),
-              Text('$numOfPlayers / $maxPlayers',
-                  style: Theme.of(context).textTheme.subtitle1),
-            ],
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2,),
+              child: Container(
+                color: AppStyles.blackShadowOp20,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('IP: ${server.serverIp}:${server.serverPort}',
+                    style: Theme.of(context).textTheme.headline6),
+                const SizedBox(height: 55,),
+                Text('Current Map: $map', style: Theme.of(context).textTheme.subtitle1),
+                const SizedBox(height: 5,),
+                Text('Players: $numOfPlayers / $maxPlayers',
+                    style: Theme.of(context).textTheme.subtitle1),
+              ],
+            ),
           ),
         ],
       ),
