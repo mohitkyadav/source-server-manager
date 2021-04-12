363dbd251f29129013ecc406cab186d8af99fe87
Mohit K. Yadav <mohitkyadav@outlook.com>
2021-04-12 09:27:41 +0530

    slide-able dismissal

--- lib/ui/pages/home/server_item.dart
+++ lib/ui/pages/home/server_item.dart
@@ -19,43 +19,55 @@ class ServerItem extends StatelessWidget {
       },
       child: Material(
         elevation: 12,
-        child: Slidable(
-          actionPane: const SlidableDrawerActionPane(),
-          showAllActionsThreshold: 1,
-          secondaryActions: <Widget>[
-            IconSlideAction(
-                icon: Icons.delete,
-                color: AppStyles.red,
-                onTap: () {
-                  print('remove sv');
-                }
+        child: Container(
+          decoration: BoxDecoration(
+            color: AppStyles.charcoalGrey,
+            gradient: const LinearGradient(
+              begin: Alignment(-4, 0.1),
+              end: Alignment(1, 0.1),
+              colors: <Color>[AppStyles.blue, AppStyles.charcoalGrey],
             ),
-          ],
-          child: Container(
-            padding: const EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              color: AppStyles.charcoalGrey,
-              gradient: const LinearGradient(
-                begin: Alignment(-4, 0.1),
-                end: Alignment(1, 0.1),
-                colors: <Color>[AppStyles.blue, AppStyles.charcoalGrey],
-              ),
-              border: Border(
-                bottom: BorderSide(
-                    width: 4, color: Theme.of(context).accentColor),
+            border: Border(
+              bottom: BorderSide(
+                  width: 4, color: Theme.of(context).accentColor),
+            ),
+          ),
+          child: Dismissible(
+            key: ObjectKey(server.serverIp),
+            background: Container(
+              padding: const EdgeInsets.only(left: 35, right: 35),
+              color: AppStyles.red.withOpacity(0.8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Icon(
+                    Icons.delete,
+                    color: AppStyles.white.withOpacity(0.9),
+                  ),
+                  Icon(
+                    Icons.delete,
+                    color: AppStyles.white.withOpacity(0.9),
+                  ),
+                ],
               ),
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(server.serverName, style: AppStyles.serverItemTitle,),
-                const SizedBox(height: 8,),
-                Text('${server.serverIp} ${server.serverPort}',
-                  style: AppStyles.serverItemSubTitle,
-                ),
-                const SizedBox(height: 8,),
-                Text(server.serverGame, style: AppStyles.serverItemSubTitle),
-              ],
+            onDismissed: (DismissDirection dir) {
+              print('remove');
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(server.serverName, style: AppStyles.serverItemTitle,),
+                  const SizedBox(height: 8,),
+                  Text('${server.serverIp} ${server.serverPort}',
+                    style: AppStyles.serverItemSubTitle,
+                  ),
+                  const SizedBox(height: 8,),
+                  Text(server.serverGame, style: AppStyles.serverItemSubTitle),
+                ],
+              ),
             ),
           ),
         ),
