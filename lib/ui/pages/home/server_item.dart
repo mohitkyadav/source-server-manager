e0422efcdc104b7bbd61f6c66a5a840d74e4ccc6
Mohit K. Yadav <mohitkyadav@outlook.com>
2021-04-13 13:20:33 +0530

    style dissmissble

--- lib/ui/pages/home/server_item.dart
+++ lib/ui/pages/home/server_item.dart
@@ -13,50 +13,47 @@ class ServerItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.pushNamed(context, serverDetailsRoute, arguments: server);
-      },
-      onLongPress: () {
-        handleSvLongPress(server);
-      },
-      child: Material(
-        elevation: 12,
-        child: Container(
-          decoration: BoxDecoration(
-            color: AppStyles.charcoalGrey,
-            gradient: const LinearGradient(
-              begin: Alignment(-4, 0.1),
-              end: Alignment(1, 0.1),
-              colors: <Color>[AppStyles.blue, AppStyles.charcoalGrey],
+    return Dismissible(
+      key: ObjectKey(server.serverIp),
+      background: Container(
+        padding: const EdgeInsets.only(left: 35, right: 35),
+        color: AppStyles.red.withOpacity(0.8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Icon(
+              Icons.delete,
+              color: AppStyles.white.withOpacity(0.9),
             ),
-            border: Border(
-              bottom: BorderSide(
-                  width: 4, color: Theme.of(context).accentColor),
-            ),
-          ),
-          child: Dismissible(
-            key: ObjectKey(server.serverIp),
-            background: Container(
-              padding: const EdgeInsets.only(left: 35, right: 35),
-              color: AppStyles.red.withOpacity(0.8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Icon(
-                    Icons.delete,
-                    color: AppStyles.white.withOpacity(0.9),
-                  ),
-                  Icon(
-                    Icons.delete,
-                    color: AppStyles.white.withOpacity(0.9),
-                  ),
-                ],
+          ],
+        ),
+      ),
+      onDismissed: (DismissDirection dir) {
+        _removeServer(server);
+      },
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, serverDetailsRoute, arguments: server);
+        },
+        onLongPress: () {
+          handleSvLongPress(server);
+        },
+        child: Material(
+          elevation: 12,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: AppStyles.charcoalGrey,
+              gradient: const LinearGradient(
+                begin: Alignment(-4, 0.1),
+                end: Alignment(1, 0.1),
+                colors: <Color>[AppStyles.blue, AppStyles.charcoalGrey],
+              ),
+              border: Border(
+                bottom: BorderSide(
+                    width: 4, color: Theme.of(context).accentColor),
               ),
             ),
-            onDismissed: (DismissDirection dir) {
-              _removeServer(server);
-            },
             child: Padding(
               padding: const EdgeInsets.all(15.0),
               child: Column(
