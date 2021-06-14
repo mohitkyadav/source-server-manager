e61f4289b1627204f1ff79e8a68fe91437112375
Mohit K. Yadav <mohitkyadav@outlook.com>
2021-06-14 13:32:38 +0530

    create ui for saved commands

--- lib/ui/widgets/console.dart
+++ lib/ui/widgets/console.dart
@@ -11,7 +11,6 @@ class Console extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController _cmdInput = TextEditingController();
 
     return Container(
       color: AppStyles.black,
@@ -36,60 +35,104 @@ class Console extends StatelessWidget {
                 ),
               )
           ),
-          Container(
-            decoration: const BoxDecoration(
-              color: AppStyles.blackShadowOp50,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(15),
-                topRight: Radius.circular(15),
-              )
-            ),
-            padding: const EdgeInsets.only(top: 20, right: 5, left: 10,
-                bottom: 35),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
+          _buildInputSection(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInputSection (BuildContext context) {
+    final TextEditingController _cmdInput = TextEditingController();
+
+    return Container(
+      decoration: const BoxDecoration(
+          color: AppStyles.blackShadowOp50,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(15),
+            topRight: Radius.circular(15),
+          )
+      ),
+      padding: const EdgeInsets.only(top: 20, right: 5, left: 10,
+          bottom: 20),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Expanded(
+                child: TextField(
                     controller: _cmdInput,
                     decoration: AppStyles.playerActionInputDec(
                         '', 'Command to server'
                     )
+                ),
+              ),
+              const SizedBox(width: 15,),
+              ClipOval(
+                child: Material(
+                  color: AppStyles.white20,
+                  child: InkWell(
+                    splashColor: AppStyles.blue2,
+                    onTap: () {
+                      sendCommandToSv(_cmdInput.text);
+                    },
+                    child: const SizedBox(
+                        width: 40, height: 40, child: Icon(Icons.add)),
                   ),
                 ),
-                const SizedBox(width: 15,),
-                ClipOval(
-                  child: Material(
-                    color: AppStyles.white20,
-                    child: InkWell(
-                      splashColor: AppStyles.blue2,
-                      onTap: () {
-                        sendCommandToSv(_cmdInput.text);
-                      },
-                      child: const SizedBox(
-                          width: 40, height: 40, child: Icon(Icons.add)),
-                    ),
+              ),
+              const SizedBox(width: 15,),
+              ClipOval(
+                child: Material(
+                  color: AppStyles.white20,
+                  child: InkWell(
+                    splashColor: AppStyles.blue2,
+                    onTap: () {
+                      sendCommandToSv(_cmdInput.text);
+                    },
+                    child: const SizedBox(
+                        width: 40, height: 40, child: Icon(Icons.send)),
                   ),
                 ),
-                const SizedBox(width: 15,),
-                ClipOval(
-                  child: Material(
-                    color: AppStyles.white20,
-                    child: InkWell(
-                      splashColor: AppStyles.blue2,
-                      onTap: () {
-                        sendCommandToSv(_cmdInput.text);
-                      },
-                      child: const SizedBox(
-                          width: 40, height: 40, child: Icon(Icons.send)),
-                    ),
-                  ),
-                )
-              ],
-            ),
+              )
+            ],
           ),
+          const SizedBox(height: 10,),
+          _buildSavedChips(context),
         ],
       ),
+    );
+  }
+
+  Widget _buildSavedChips (BuildContext context) {
+    return Row(
+      children: <Widget>[
+        InputChip(
+          padding: const EdgeInsets.all(2.0),
+          label: const Text('status',
+              style: TextStyle(color:AppStyles.white)),
+          backgroundColor: AppStyles.blue2,
+          onSelected: (bool selected) {
+            print('selected');
+          },
+          onDeleted: () {
+            print('deleted');
+          },
+        ),
+        const SizedBox(width: 20,),
+        InputChip(
+          padding: const EdgeInsets.all(2.0),
+          label: const Text('plugin_print',
+              style: TextStyle(color:AppStyles.white)),
+          backgroundColor: AppStyles.blue2,
+          onSelected: (bool selected) {
+            print('selected');
+          },
+          onDeleted: () {
+            print('deleted');
+          },
+        ),
+      ],
     );
   }
 }
