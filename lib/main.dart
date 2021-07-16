bd085f41f1a1477f054f60fc1161ce34d23abbaf
Mohit K. Yadav <mohitkyadav@outlook.com>
2021-07-16 10:33:48 +0530

    fix min size

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ void main () {
     WidgetsFlutterBinding.ensureInitialized();
     if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
       setWindowTitle('Source Server Manager');
-      // setWindowMinSize(const Size(540, 900));
+      setWindowMinSize(const Size(540, 900));
       // setWindowMaxSize(const Size(680, 900));
     }
     runApp(MyApp(darkModeOn, selectedLocale));
