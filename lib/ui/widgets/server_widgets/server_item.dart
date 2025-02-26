9314c7498844988cda947f49a3720a3998e9808e
Mohit Kumar Yadav <mohit.yadav@cobalt.io>
2025-02-26 10:52:26 +0530

    Upgrade provider and slidable

--- lib/ui/widgets/server_widgets/server_item.dart
+++ lib/ui/widgets/server_widgets/server_item.dart
@@ -46,126 +46,73 @@ class _ServerItemState extends State<ServerItem> {
   @override
   Widget build(BuildContext context) {
 
-    return Slidable(
-      key: Key('${widget.server.serverIp}:${widget.server.serverName}'),
-      actionPane: const SlidableBehindActionPane(),
-      actionExtentRatio: 0.25,
-      child: InkWell(
-        onTap: () {
-          final bool isSmallScreen = MediaQuery.of(context).size.width < 900;
+    return InkWell(
+      onTap: () {
+        final bool isSmallScreen = MediaQuery.of(context).size.width < 900;
 
-          if (isSmallScreen) {
-            Navigator.pushNamed(context, serverDetailsRoute,
-                arguments: widget.server);
-          }
-          widget._setSelectedServer(widget.server);
-        },
-        onLongPress: () {
-          widget._handleSvLongPress(widget.server);
-        },
-        child: Material(
-          elevation: 8,
-          shadowColor: (_isOffline ? AppStyles.red : AppStyles.blue2)
-              .withOpacity(0.1),
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              color: (_isOffline ? AppStyles.red : AppStyles.blue2)
-                  .withOpacity(0.2),
-              borderRadius: const BorderRadius.all(Radius.circular(5)),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Flexible(
-                        child: Text(widget.server.serverName!,
-                          overflow: TextOverflow.ellipsis,
-                          style: AppStyles.serverItemTitle,),
-                      ),
-                      if(widget.server.serverRcon != null) const Icon(
-                        Icons.lock_open_rounded, size: 16,
-                          color: AppStyles.white40),
-                    ],
-                  ),
-                  const SizedBox(height: 8,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        if (isSmallScreen) {
+          Navigator.pushNamed(context, serverDetailsRoute,
+              arguments: widget.server);
+        }
+        widget._setSelectedServer(widget.server);
+      },
+      onLongPress: () {
+        widget._handleSvLongPress(widget.server);
+      },
+      child: Material(
+        elevation: 8,
+        shadowColor: (_isOffline ? AppStyles.red : AppStyles.blue2)
+            .withOpacity(0.1),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: (_isOffline ? AppStyles.red : AppStyles.blue2)
+                .withOpacity(0.2),
+            borderRadius: const BorderRadius.all(Radius.circular(5)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          const Icon(Icons.group, size: 20,
-                            color: AppStyles.white40,),
-                          const SizedBox(width: 5,),
-                          Text(playerInfo, style: AppStyles.serverItemSubTitle),
-                        ],
-                      ),
-                      if (_isTvEnabled)
-                        const Icon(Icons.tv, size: 16,
+                    Flexible(
+                      child: Text(widget.server.serverName!,
+                        overflow: TextOverflow.ellipsis,
+                        style: AppStyles.serverItemTitle,),
+                    ),
+                    if(widget.server.serverRcon != null) const Icon(
+                        Icons.lock_open_rounded, size: 16,
+                        color: AppStyles.white40),
+                  ],
+                ),
+                const SizedBox(height: 8,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        const Icon(Icons.group, size: 20,
                           color: AppStyles.white40,),
-                      if (_isOffline)
-                        const Icon(Icons.signal_wifi_off_sharp, size: 16,
-                          color: AppStyles.white40,),
-                    ],
-                  ),
-                ],
-              ),
+                        const SizedBox(width: 5,),
+                        Text(playerInfo, style: AppStyles.serverItemSubTitle),
+                      ],
+                    ),
+                    if (_isTvEnabled)
+                      const Icon(Icons.tv, size: 16,
+                        color: AppStyles.white40,),
+                    if (_isOffline)
+                      const Icon(Icons.signal_wifi_off_sharp, size: 16,
+                        color: AppStyles.white40,),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
       ),
-      actions: <Widget>[
-        SlideAction(
-            child: Container(
-                width: 105,
-                margin: const EdgeInsets.only(right: 5),
-                decoration: const BoxDecoration(
-                  color: AppStyles.red,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(5.0),
-                  ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const Icon(Icons.clear),
-                    Text(AppLocalizations.of(context)!
-                        .getTranslatedValue('delete_sv_msg')!,
-                      style: AppStyles.serverItemActionText,)
-                  ],
-                )
-            ),
-            onTap: () => widget._removeServer(widget.server)
-        ),
-      ],
-      secondaryActions: <Widget>[
-        SlideAction(
-            child: Container(
-                width: 105,
-                margin: const EdgeInsets.only(left: 5),
-                decoration: const BoxDecoration(
-                  color: AppStyles.lightPurple,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(5.0),
-                  ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const Icon(Icons.edit),
-                    Text(AppLocalizations.of(context)!
-                        .getTranslatedValue('edit_sv_msg')!,
-                        style: AppStyles.serverItemActionText)
-                  ],
-                )
-            ),
-            onTap: () => widget._handleSvLongPress(widget.server)
-        ),
-      ],
     );
   }
 
